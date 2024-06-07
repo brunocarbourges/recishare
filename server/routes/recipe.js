e6ce70d2e43ac3b69254d2a244d8f88d0977cdca
Bruno Cardenas-Bourges <brunocarbourges@gmail.com>
2024-06-06 23:00:54 -0700

    Added userRecipeService

--- server/routes/recipe.js
+++ server/routes/recipe.js
@@ -31,7 +31,7 @@ router.get('/bestrec', p_auth, getBestRecipes);
 // router.get('/tags', p_auth, getBestRecipes);
 
 router.get('/saved/:userID', validate(getSavedRecipeSchema), getSavedRecipes);
-router.get('/user/:userID', p_auth, validate(getUserRecipesSchema), getUserRecipes);
+router.get('/user/:userID', validate(getUserRecipesSchema), getUserRecipes);
 router.get('/following/:userID', validate(getFollowingRecipesSchema), getFollowingRecipes);
 
 router.get('/:id', p_auth, validate(getOneRecipeSchema), getOneRecipe);
